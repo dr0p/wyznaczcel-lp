c8dd02402ecb2852c617ca1816e2f5b6f73fb7c8
Paweł Drożański <pawdro@gmail.com>
2014-01-19 01:15:09 +0100

    poprawki layoutu, bardzo wstępna walidacja maila

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
-  attr_accessible :email
+	attr_accessible :email
+
+	validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i }
+	validates :email, presence: true
+
 end
