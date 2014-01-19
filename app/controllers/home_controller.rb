a86650dbc40f3bc9699771b07bb4a23d23b14103
Paweł Drożański <pawdro@gmail.com>
2014-01-19 18:13:59 +0100

    new lp tweaks

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
     @user = User.new
-    @ab_submit_button_type = ab_test("submit_button_type", ['warning', 'danger'])
+    # @ab_submit_button_type = ab_test("submit_button_type", ['warning', 'danger'])
   end
 end
